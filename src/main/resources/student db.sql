022721994c7383692a1bd68b8e2be9c69aa40b49
Vaibhav Diwan <vaibhav.diwan@niit.com>
2023-01-31 17:41:01 +0530

    added sql file for data

---
+++ src/main/resources/student db.sql
@@ -0,0 +1,37 @@
+/*
+-- Query: select * from student
+LIMIT 0, 50000
+
+-- Date: 2023-01-31 17:39
+*/
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (2,'Ramesh',39,'Bhopal',500);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (NULL,'Suresh',39,'Bhopal',500);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (5,'Aswin',23,'Kerala',99);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (7,'Harsh',21,'UP',43);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (11,'Dinesh',99,'karnal',500);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (1007,'Ezhilarasan',24,'Dharmapuri',477);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (6,'Mahek',21,'Maharashtra',97);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (26,'Shivam',21,'lucknow',350);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (1,'John',18,'Tuty',80);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (8,'Harika',22,'Chennai',100);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (3,'Aman',23,'Delhi',90);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (7,'Mani',22,'Chennai',464);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (10,'Anitha',19,'Bangalore',90);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (17,'Ashutosh',100,'Bangalore',444);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (22,'Avinash',22,'Chennai',440);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (101,'hemangi',25,'nagpur',676);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (16,'LOKESH',22,'Chennai',35);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (11,'kotresh',25,'bengalur',400);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (26,'Sanket',24,'Maharashtra',75);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (25,'Hina',24,'Maharashtra',92);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (12,'vinay',22,'madhyapradesh',440);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (27,'Rishi',21,'chennai',475);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (10,'kathir',23,'Theni',80);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (30,'kavya',25,'Bhadrachalam',90);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (11,'krishna',25,'akluj',70);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (1,'Jesu',23,'Thoothukudi',80);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (20,'ghanshyam',20,'bihar',80);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (NULL,'ghanshyam',20,'bihar',80);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (899,'Ravi Kumar',15,'Indore',545);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (454,'Aryan Sharma',12,'BHopal',500);
+INSERT INTO `` (`rollno`,`sname`,`age`,`address`,`marks`) VALUES (12,'tttt',55,'tttt',450);
